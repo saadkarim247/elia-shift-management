49e9c0b5d1f25488ea982f2ac834e0938926ba01
Maaz-24503 <saadkarim247@gmail.com>
2024-05-04 13:59:45 +0500

    Testing git changes

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.red,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('My name is saad'),
+        title: Text('Testing git changes'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
